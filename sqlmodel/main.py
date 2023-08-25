d13fb7435bf9bdcbca4671dcc5935f8c0e4adf45
honglei <jhonglei@gmail.com>
2023-08-25 21:43:02 +0800

    only use typing_extensions to import _AnnotatedAlias

--- sqlmodel/main.py
+++ sqlmodel/main.py
@@ -55,10 +55,7 @@ if sys.version_info >= (3, 8):
 else:
     from typing_extensions import get_args, get_origin
 
-if sys.version_info >= (3, 9):
-    from typing import Annotated, _AnnotatedAlias
-else:
-    from typing_extensions import Annotated, _AnnotatedAlias
+from typing_extensions import Annotated, _AnnotatedAlias
 
 _T = TypeVar("_T")
 NoArgAnyCallable = Callable[[], Any]
